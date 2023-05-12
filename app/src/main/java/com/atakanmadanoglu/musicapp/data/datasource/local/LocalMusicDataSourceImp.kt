a9e93fc424071e6068d3c2990ee21a1ac7d503fd
Atakan Madanoğlu <ati64.98.35@gmail.com>
2023-05-12 20:46:57 +0300

    Edited local and remote dataSource files

--- app/src/main/java/com/atakanmadanoglu/musicapp/data/datasource/local/LocalMusicDataSourceImp.kt
+++ app/src/main/java/com/atakanmadanoglu/musicapp/data/datasource/local/LocalMusicDataSourceImp.kt
@@ -1,18 +1,18 @@
 package com.atakanmadanoglu.musicapp.data.datasource.local
 
+import com.atakanmadanoglu.musicapp.data.service.local.FavoriteTrackEntity
 import com.atakanmadanoglu.musicapp.data.service.local.MusicDao
-import com.atakanmadanoglu.musicapp.data.service.local.MusicEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class LocalMusicDataSourceImp @Inject constructor(
     private val musicDao: MusicDao
 ): LocalMusicDataSource {
-    override fun getMusics(): Flow<List<MusicEntity>> = musicDao.getMusics()
+    override fun getMusics(): Flow<List<FavoriteTrackEntity>> = musicDao.getMusics()
 
     override suspend fun addMusic(
-        musicEntity: MusicEntity
-    ) = musicDao.addMusic(musicEntity)
+        favoriteTrackEntity: FavoriteTrackEntity
+    ) = musicDao.addMusic(favoriteTrackEntity)
 
     override suspend fun deleteMusicById(
         musicId: Int
--- app/src/main/java/com/atakanmadanoglu/musicapp/data/datasource/remote/RemoteMusicDataSource.kt
+++ app/src/main/java/com/atakanmadanoglu/musicapp/data/datasource/remote/RemoteMusicDataSource.kt
@@ -1,15 +1,14 @@
 package com.atakanmadanoglu.musicapp.data.datasource.remote
 
-import com.atakanmadanoglu.musicapp.data.service.remote.dto.Albums
-import com.atakanmadanoglu.musicapp.data.service.remote.dto.Artist
-import com.atakanmadanoglu.musicapp.data.service.remote.dto.Artists
-import com.atakanmadanoglu.musicapp.data.service.remote.dto.Genres
-import com.atakanmadanoglu.musicapp.data.service.remote.dto.Tracks
+import com.atakanmadanoglu.musicapp.data.service.remote.dto.AlbumDTO
+import com.atakanmadanoglu.musicapp.data.service.remote.dto.ArtistDTO
+import com.atakanmadanoglu.musicapp.data.service.remote.dto.GenreDTO
+import com.atakanmadanoglu.musicapp.data.service.remote.dto.TrackDTO
 
 interface RemoteMusicDataSource {
-    suspend fun getGenres(): Genres
-    suspend fun getArtistsByGenre(genreId: Int): Artists
-    suspend fun getArtistById(artistId: Int): Artist
-    suspend fun getArtistAlbumsById(artistId: Int): Albums
-    suspend fun getArtistTracksById(artistId: Int): Tracks
+    suspend fun getGenres(): List<GenreDTO>
+    suspend fun getArtistsByGenre(genreId: Int): List<ArtistDTO>
+    suspend fun getArtistById(artistId: Int): ArtistDTO
+    suspend fun getArtistAlbumsById(artistId: Int): List<AlbumDTO>
+    suspend fun getArtistTracksById(artistId: Int): List<TrackDTO>
 }

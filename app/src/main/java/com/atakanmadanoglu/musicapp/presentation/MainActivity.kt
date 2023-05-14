97011952c517b79fec9fdca605889b11c9e9f1c3
Atakan Madanoğlu <ati64.98.35@gmail.com>
2023-05-14 17:51:03 +0300

    Moved NavHost operations to another file

--- app/src/main/java/com/atakanmadanoglu/musicapp/presentation/MainActivity.kt
+++ app/src/main/java/com/atakanmadanoglu/musicapp/presentation/MainActivity.kt
@@ -8,17 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.atakanmadanoglu.musicapp.presentation.album_details.navigation.albumDetailsScreen
-import com.atakanmadanoglu.musicapp.presentation.album_details.navigation.navigateToAlbumDetails
-import com.atakanmadanoglu.musicapp.presentation.artist_details.navigation.artistDetailsScreen
-import com.atakanmadanoglu.musicapp.presentation.artist_details.navigation.navigateToArtistDetailsScreen
-import com.atakanmadanoglu.musicapp.presentation.artist_list.navigation.artistListScreen
-import com.atakanmadanoglu.musicapp.presentation.artist_list.navigation.navigateToArtistListScreen
-import com.atakanmadanoglu.musicapp.presentation.favorite_tracks.navigation.favoriteTracksScreen
-import com.atakanmadanoglu.musicapp.presentation.music_categories.navigation.musicCategoriesScreen
+import com.atakanmadanoglu.musicapp.presentation.navigation.MusicAppNavHost
 import com.atakanmadanoglu.musicapp.presentation.navigation.Screen
 import com.atakanmadanoglu.musicapp.theme.MusicAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -40,37 +32,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.MusicCategoryScreen.route
-                    ) {
-                        musicCategoriesScreen(
-                            currentRoute = currentRoute,
-                            onBottomNavItemClicked = { route ->
-                                navController.navigate(route)
-                            },
-                            onCardClicked = { genreId, genreName ->
-                                navController.navigateToArtistListScreen(genreId, genreName)
-                            }
-                        )
-                        artistListScreen(
-                            onCardClicked = { id ->
-                                navController.navigateToArtistDetailsScreen(id)
-                            }
-                        )
-                        favoriteTracksScreen(
-                            currentRoute = currentRoute,
-                            onBottomNavItemClicked = {
-                                navController.navigate(it)
-                            }
-                        )
-                        artistDetailsScreen(
-                            onCardClicked = { id->
-                                navController.navigateToAlbumDetails(id)
-                            }
-                        )
-                        albumDetailsScreen()
-                    }
+                   MusicAppNavHost(
+                       navController = navController,
+                       currentRoute = currentRoute
+                   )
                 }
             }
         }
